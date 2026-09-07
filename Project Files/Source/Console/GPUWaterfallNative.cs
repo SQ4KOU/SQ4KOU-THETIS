5b8351485ce947f0c05b5f527fcc7059ad701998
SQ4KOU <sq4kou@wp.pl>
2026-09-07 19:39:34 +0200

    GPU waterfall: add managed ChannelMaster bridge

---
+++ Project Files/Source/Console/GPUWaterfallNative.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Runtime.InteropServices;
+
+namespace Thetis
+{
+    internal static class GPUWaterfallNative
+    {
+        private const string DllName = "ChannelMaster.dll";
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int CM_GPUWaterfall_Init(int channel, int fftSize, int ringCapacity);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int CM_GPUWaterfall_Process(int channel, int displayWidth, int sampleRate,
+            float displayLowHz, float displayHighHz, [Out] float[] outputDb);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void CM_GPUWaterfall_Free(int channel);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern int CM_GPUWaterfall_IsReady(int channel);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern ulong CM_WaterfallIQ_DroppedSamples(int channel);
+
+        [DllImport(DllName, CallingConvention = CallingConvention.Cdecl)]
+        internal static extern void CM_WaterfallIQ_ResetDropped(int channel);
+    }
+}
