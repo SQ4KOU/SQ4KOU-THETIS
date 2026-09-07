b5d60d7cf29e1cb2e0368916da002f5bad8ddf93
SQ4KOU <sq4kou@wp.pl>
2026-09-07 19:37:50 +0200

    GPU waterfall: add DirectCompute API

---
+++ Project Files/Source/ChannelMaster/gpu_waterfall.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+__declspec(dllexport) int __cdecl CM_GPUWaterfall_Init(int channel, int fftSize, int ringCapacity);
+__declspec(dllexport) int __cdecl CM_GPUWaterfall_Process(int channel, int displayWidth, int sampleRate, float displayLowHz, float displayHighHz, float* outputDb);
+__declspec(dllexport) void __cdecl CM_GPUWaterfall_Free(int channel);
+__declspec(dllexport) int __cdecl CM_GPUWaterfall_IsReady(int channel);
+
+#ifdef __cplusplus
+}
+#endif
