bcba5c7b258ff2fbcb34a047d0425c4672de6a5c
SQ4KOU <sq4kou@wp.pl>
2026-09-07 20:32:50 +0200

    GPU waterfall V3: expose Extended processing controls

--- Project Files/Source/ChannelMaster/gpu_waterfall.h
+++ Project Files/Source/ChannelMaster/gpu_waterfall.h
@@ -5,7 +5,10 @@ extern "C" {
 #endif
 
 __declspec(dllexport) int __cdecl CM_GPUWaterfall_Init(int channel, int fftSize, int ringCapacity);
-__declspec(dllexport) int __cdecl CM_GPUWaterfall_Process(int channel, int displayWidth, int sampleRate, float displayLowHz, float displayHighHz, float* outputDb);
+__declspec(dllexport) int __cdecl CM_GPUWaterfall_Configure(int channel, int windowType, float kaiserBeta,
+    int magnitudeMode, int autoOverlap, float overlapPercent, int lanczosWindow, int resamplingMode);
+__declspec(dllexport) int __cdecl CM_GPUWaterfall_Process(int channel, int displayWidth, int sampleRate,
+    float displayLowHz, float displayHighHz, float* outputDb);
 __declspec(dllexport) void __cdecl CM_GPUWaterfall_Free(int channel);
 __declspec(dllexport) int __cdecl CM_GPUWaterfall_IsReady(int channel);
 
