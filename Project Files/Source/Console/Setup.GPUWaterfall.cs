b355e48c364aba7e0710cf2ba830214cdde8dbcf
SQ4KOU <sq4kou@wp.pl>
2026-09-25 21:24:13 +0200

    Revert run169 runtime changes to run165 diagnostic baseline

--- Project Files/Source/Console/Setup.GPUWaterfall.cs
+++ Project Files/Source/Console/Setup.GPUWaterfall.cs
@@ -1253,13 +1253,9 @@ namespace Thetis
 
 	private void chkGPUWaterfallAutoOverlap_CheckedChanged(object sender, EventArgs e)
 	{
-		bool auto = chkGPUWaterfallAutoOverlap != null && chkGPUWaterfallAutoOverlap.Checked;
-		if (udGPUWaterfallOverlap != null) udGPUWaterfallOverlap.Enabled = !auto;
-		if (lblGPUWaterfallEffectiveOverlap != null)
-			lblGPUWaterfallEffectiveOverlap.Text = auto ? "Eff: --%" : "";
 		if (!initializing)
 		{
-			Display.GPUWaterfallAutoOverlap = auto;
+			Display.GPUWaterfallAutoOverlap = chkGPUWaterfallAutoOverlap.Checked;
 		}
 	}
 
